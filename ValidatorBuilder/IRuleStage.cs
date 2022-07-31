3d57d1c8b866cc5c071c175c8c7ef8211702b16c
Fernando Stockler de Lima <fernando.stockler@outlook.com.br>
2022-07-31 05:11:12 -0300

    IRuleStage, IRulesForStage added.

---
+++ ValidatorBuilder/IRuleStage.cs
@@ -0,0 +1,31 @@
+﻿namespace ValidatorBuilder;
+
+using ValidatorBuilder.Core;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IRuleStage
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    IRuleStage Rule(string? message, Predicate<string?>? predicate);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    IRuleStage RulesFor(string? key, string? message);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    Validator Build();
+}
---
+++ ValidatorBuilder/IRulesForStage.cs
@@ -0,0 +1,15 @@
+﻿namespace ValidatorBuilder;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IRulesForStage
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    IRuleStage RulesFor(string? key, string? message);
+}
