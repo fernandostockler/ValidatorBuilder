45e51fa1bf8cf6622ecce7707ae43fd3db7c1797
Fernando Stockler de Lima <fernando.stockler@outlook.com.br>
2022-07-31 04:56:48 -0300

    IValidator and RuleGroupChangeEventArgs added.

---
+++ ValidatorBuilder/Core/IValidator.cs
@@ -0,0 +1,26 @@
+﻿namespace ValidatorBuilder.Core;
+
+/// <summary>
+/// Represents a validator.
+/// </summary>
+public interface IValidator
+{
+    /// <summary>
+    /// Occurs when the rule group changes.
+    /// </summary>
+    event EventHandler<RuleGroupChangedEventArgs>? RuleGroupChangedEvent;
+
+    /// <summary>
+    /// Returns a collection of rules registered to a given key.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>A <see cref="RuleGroup"/> object.</returns>
+    RuleGroup RulesFor(string? key);
+
+    /// <summary>
+    /// Validates a string value through the predicate associated with the key.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="valueToValidade">The value to be evaluated by the predicate.</param>
+    void ValidateFor(string? key, string? valueToValidade);
+}
---
+++ ValidatorBuilder/Core/RuleGroupChangedEventArgs.cs
@@ -0,0 +1,28 @@
+﻿namespace ValidatorBuilder.Core;
+
+/// <summary>
+/// Represents a change in the RuleGroup.
+/// </summary>
+public class RuleGroupChangedEventArgs : EventArgs
+{
+    /// <summary>
+    /// A Key that identifies a RuleGroup.
+    /// </summary>
+    public string? Key { get; init; }
+
+    /// <summary>
+    /// <inheritdoc cref="RuleGroup"/>
+    /// </summary>
+    public RuleGroup? RuleGroup { get; init; }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="ruleGroup"></param>
+    public RuleGroupChangedEventArgs(string? key, RuleGroup? ruleGroup)
+    {
+        Key = key;
+        RuleGroup = ruleGroup;
+    }
+}
