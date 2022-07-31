b1b4568cebb9b68fba86c559f1ed8450906253e6
Fernando Stockler de Lima <fernando.stockler@outlook.com.br>
2022-07-31 03:51:45 -0300

    IRule added.

---
+++ ValidatorBuilder/Core/IRule.cs
@@ -0,0 +1,29 @@
+﻿namespace ValidatorBuilder.Core;
+
+/// <summary>
+/// Represents a validation rule that will be applied to a given value.
+/// </summary>
+public interface IRule
+{
+    /// <summary>
+    /// A message explaining the validation rule.
+    /// </summary>
+    string? Description { get; set; }
+
+    /// <summary>
+    /// <inheritdoc cref="Predicate{T}"/>
+    /// </summary>
+    Predicate<string?>? Filter { get; set; }
+
+    /// <summary>
+    /// Validates a string value through the predicate.
+    /// </summary>
+    /// <param name="s">The <see cref="string"/> value to be evaluated</param>
+    /// <returns></returns>
+    bool? Validate(string? s);
+
+    /// <summary>
+    /// <see langword="true"/> if it's a valid value, otherwise returns <see langword="false"/>.
+    /// </summary>
+    bool? IsValid { get; set; }
+}
