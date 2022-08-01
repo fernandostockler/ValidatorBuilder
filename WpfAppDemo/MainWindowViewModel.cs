198a3c2909ec8d3c22ddd28ddb391fea8278b90d
Fernando Stockler de Lima <fernando.stockler@outlook.com.br>
2022-08-01 06:31:57 -0300

    short code

--- WpfAppDemo/MainWindowViewModel.cs
+++ WpfAppDemo/MainWindowViewModel.cs
@@ -7,45 +7,26 @@ using ValidatorBuilder.Core;
 public class MainWindowViewModel : ViewModelBase
 {
     const string Email = "Email";
-
-    const int EmailMinLength = 7;
-    const int EmailMaxLength = 100;
+    const string Password = "Password";
     const string EmailPattern = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
 
-    static readonly Predicate<string?> EmailMinLenghtFilter = x => x?.Length >= EmailMinLength;
-    static readonly Predicate<string?> EmailMaxLenghtFilter = x => x?.Length <= EmailMaxLength;
-    static readonly Predicate<string?> EmailFormatFilter = x => x is not null && new Regex(EmailPattern).Match(x).Success;
-
-    const string Password = "Password";
-
-    const int PasswordMinLength = 8;
-    const int PasswordMaxLength = 16;
-
-    static readonly Predicate<string?> PasswordMinLenghtFilter = x => x?.Length >= PasswordMinLength;
-    static readonly Predicate<string?> PasswordMaxLenghtFilter = x => x?.Length <= PasswordMaxLength;
-    static readonly Predicate<string?> PasswordNumberFilter = x => x is not null && x.ToCharArray().Any(c => char.IsNumber(c));
-    static readonly Predicate<string?> PasswordLetterFilter = x => x is not null && x.ToCharArray().Any(c => char.IsLetter(c));
-    static readonly Predicate<string?> PasswordCapsFilter = x => x is not null && x.ToCharArray().Any(c => char.IsUpper(c));
-    static readonly Predicate<string?> PasswordSymbolFilter = x => x is not null && x.IndexOfAny(new[] { '!', '@', '#', '%', '&', '*', '?' }) > -1;
-
     readonly Validator Validator = ValidatorBuilder
-        .Create()
-            .RulesFor(Email, "Email requirements:")
-                .Rule($"Must contain at least {EmailMinLength} characters.", EmailMinLenghtFilter)
-                .Rule($"Must contain at most {EmailMaxLength} characters.", EmailMaxLenghtFilter)
-                .Rule("The email must be in a valid format.", EmailFormatFilter)
+    .Create()
+        .RulesFor(Email, "Email requirements:")
+            .Rule($"Must contain at least 7 characters.", x => x?.Length >= 7)
+            .Rule($"Must contain at most 100 characters.", x => x?.Length <= 100)
+            .Rule("The email must be in a valid format.", x => x is not null && new Regex(EmailPattern).Match(x).Success)
 
-            .RulesFor(Password, "Password requirements:")
-                .Rule($"Must be at least {PasswordMinLength} characters.", PasswordMinLenghtFilter)
-                .Rule($"Must be at most {PasswordMaxLength} characters.", PasswordMaxLenghtFilter)
-                .Rule("Must contain number.", PasswordNumberFilter)
-                .Rule("Must contain letter.", PasswordLetterFilter)
-                .Rule("Must be capitalized.length", PasswordCapsFilter)
-                .Rule("Must contain symbol.", PasswordSymbolFilter)
+        .RulesFor(Password, "Password requirements:")
+            .Rule($"Must be at least 8 characters.", x => x?.Length >= 8)
+            .Rule($"Must be at most 16 characters.", x => x?.Length <= 16)
+            .Rule("Must contain number.", x => x is not null && x.ToCharArray().Any(c => char.IsNumber(c)))
+            .Rule("Must contain letter.", x => x is not null && x.ToCharArray().Any(c => char.IsLetter(c)))
+            .Rule("Must be capitalized.length", x => x is not null && x.ToCharArray().Any(c => char.IsUpper(c)))
+            .Rule("Must contain symbol.", x => x is not null && x.IndexOfAny(new[] { '!', '@', '#', '%', '&', '*', '?' }) > -1)
 
-            .RulesFor("reset", string.Empty)
-
-        .Build();
+        .RulesFor("reset", string.Empty)
+    .Build();
 
     RuleGroup? _ruleGroup;
     string? _header;
@@ -89,4 +70,40 @@ public class MainWindowViewModel : ViewModelBase
         Header = RuleGroup.Header;
         Validator.ValidateFor("reset", string.Empty);
     }
+
+    //const int EmailMinLength = 7;
+    //const int EmailMaxLength = 100;
+
+    //static readonly Predicate<string?> EmailMinLenghtFilter = x => x?.Length >= EmailMinLength;
+    //static readonly Predicate<string?> EmailMaxLenghtFilter = x => x?.Length <= EmailMaxLength;
+    //static readonly Predicate<string?> EmailFormatFilter = x => x is not null && new Regex(EmailPattern).Match(x).Success;
+
+    //const int PasswordMinLength = 8;
+    //const int PasswordMaxLength = 16;
+
+    //static readonly Predicate<string?> PasswordMinLenghtFilter = x => x?.Length >= PasswordMinLength;
+    //static readonly Predicate<string?> PasswordMaxLenghtFilter = x => x?.Length <= PasswordMaxLength;
+    //static readonly Predicate<string?> PasswordNumberFilter = x => x is not null && x.ToCharArray().Any(c => char.IsNumber(c));
+    //static readonly Predicate<string?> PasswordLetterFilter = x => x is not null && x.ToCharArray().Any(c => char.IsLetter(c));
+    //static readonly Predicate<string?> PasswordCapsFilter = x => x is not null && x.ToCharArray().Any(c => char.IsUpper(c));
+    //static readonly Predicate<string?> PasswordSymbolFilter = x => x is not null && x.IndexOfAny(new[] { '!', '@', '#', '%', '&', '*', '?' }) > -1;
+
+    //readonly Validator Validator = ValidatorBuilder
+    //    .Create()
+    //        .RulesFor(Email, "Email requirements:")
+    //            .Rule($"Must contain at least {EmailMinLength} characters.", EmailMinLenghtFilter)
+    //            .Rule($"Must contain at most {EmailMaxLength} characters.", EmailMaxLenghtFilter)
+    //            .Rule("The email must be in a valid format.", EmailFormatFilter)
+
+    //        .RulesFor(Password, "Password requirements:")
+    //            .Rule($"Must be at least {PasswordMinLength} characters.", PasswordMinLenghtFilter)
+    //            .Rule($"Must be at most {PasswordMaxLength} characters.", PasswordMaxLenghtFilter)
+    //            .Rule("Must contain number.", PasswordNumberFilter)
+    //            .Rule("Must contain letter.", PasswordLetterFilter)
+    //            .Rule("Must be capitalized.length", PasswordCapsFilter)
+    //            .Rule("Must contain symbol.", PasswordSymbolFilter)
+
+    //        .RulesFor("reset", string.Empty)
+    //    .Build();
+
 }
